247e9120c1ec9042ab0cddad4153709e5e29cae6
hflex <haykflexgrigoryan@gmail.com>
2025-07-01 20:20:03 +0400

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ImageDownsizeIssue;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ImageDownsizeIssue;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
